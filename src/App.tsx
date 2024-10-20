73990dc56637cb0c5ee69714614bce3305999741
Xenos Warlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-10-20 15:09:47 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,8 @@ import {
 } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import dataProvider, {
-  GraphQLClient,
-  liveProvider,
-} from "@refinedev/nestjs-query";
+import { dataProvider, liveProvider } from "./providers";
+
 import routerBindings, {
   CatchAllNavigate,
   DocumentTitleHandler,
@@ -57,8 +55,8 @@ function App() {
           <AntdApp>
             <DevtoolsProvider>
               <Refine
-                dataProvider={dataProvider(gqlClient)}
-                liveProvider={liveProvider(wsClient)}
+                dataProvider={dataProvider}
+                liveProvider={liveProvider}
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
