fa25c58a541ef965123ebc290857be933a50a4e6
Xenos Warlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-10-25 21:22:57 +0530

    Add files via upload

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
 // export * from "./accordion";
 // export * from "./icon";
-// export * from "./layout";
+export * from "./layout";
 // export * from "./pagination-total";
 // export * from "./select-option-with-avatar";
 export * from "./custom-avatar";
