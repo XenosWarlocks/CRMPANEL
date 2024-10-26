c40506b0a32111561f64bc5da97c9d96ddfb5886
Xenos Warlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-10-26 14:53:57 +0530

    Add files via upload

---
+++ src/components/accordion/index.ts
@@ -0,0 +1,2 @@
+export * from "./accordion-header-skeleton";
+export * from "./accordion";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,8 @@
-// export * from "./accordion";
-// export * from "./icon";
+export * from "./accordion";
+export * from "./icon";
 export * from "./layout";
-// export * from "./pagination-total";
-// export * from "./select-option-with-avatar";
+export * from "./pagination-total";
+export * from "./select-option-with-avatar";
 export * from "./custom-avatar";
 export * from "./tags";
 export * from "./text";
