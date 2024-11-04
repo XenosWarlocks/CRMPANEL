eec525612b4ee68916ba3e072a0f7196d8169aee
Xenos Warlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-11-04 20:45:32 +0530

    Update index.tsx

--- src/routes/dashboard/components/latest-activities/index.tsx
+++ src/routes/dashboard/components/latest-activities/index.tsx
@@ -51,4 +51,132 @@ export const DashboardLatestActivities = ({ limit = 5 }: Props) => {
             gqlQuery: DASHBOARD_LATEST_ACTIVITIES_AUDITS_QUERY,
         },
     });
+
+    const dealIds = audit?.data?.map((audit) => audit.targetId);
+
+    const { data: deals, isLoading: isLoadingDeals } = useList<
+        GetFieldsFromList<DashboardLatestActivitiesDealsQuery>
+    >({
+        resource: "deals",
+        queryOptions: { enabled: !!dealIds?.length },
+        pagination: {
+            mode: "off",
+        },
+        filters: [{ field: "id", operator: "in", value: dealIds }],
+        meta: {
+            gqlQuery: DASHBOARD_LATEST_ACTIVITIES_DEALS_QUERY,
+        },
+    });
+
+    if (isError) {
+        console.error("Error fetching latest activities", error);
+        return null;
+    }
+
+    const isLoading = isLoadingAudit || isLoadingDeals;
+
+    return (
+        <Card
+            styles={{
+                header: { padding: "16px" },
+                body: { padding: "0 1rem", }
+            }}
+            title={
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "8px",
+                    }}
+                >
+                    <UnorderedListOutlined />
+                    <Text size="sm" style={{ marginLeft: ".5rem" }}>
+                        Latest activities
+                    </Text>
+                </div>
+            }
+        >
+            {isLoading ? (
+                <List
+                    itemLayout="horizontal"
+                    dataSource={Array.from({length: limit }).map((_, index) => ({
+                        id: index,
+                    }))}
+                    renderItem={(_item, index) => {
+                        return (
+                            <List.Item key={index}>
+                                <List.Item.Meta
+                                    avatar={
+                                        <AntdSkeleton.Avatar
+                                            active
+                                            size={48}
+                                            shape="square"
+                                            style={{
+                                                borderRadius: "4px",
+                                            }}
+                                        />
+                                    }
+                                    title={
+                                        <AntdSkeleton.Button
+                                            active
+                                            style={{
+                                                height: "16px",
+                                            }}
+                                        />
+                                    }
+                                    description={
+                                        <AntdSkeleton.Button
+                                            active
+                                            style={{
+                                                width: "300px",
+                                                height: "16px",
+                                            }}
+                                        />
+                                    }
+                                />
+                            </List.Item>
+                        );
+                    }}
+                />
+            ) : (
+                <List
+                    itemLayout="horizontal"
+                    dataSource={audit?.data || []}
+                    renderItem={(item) => {
+                        const deal = deals?.data.find(
+                            (deal) => deal.id === `${item.targetId}`
+                        ) || undefined;
+
+                        return (
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={
+                                        <CustomAvatar
+                                            shape="square"
+                                            size={48}
+                                            src={deal?.company.avatarUrl}
+                                            name={deal?.company.name}
+                                        />
+                                    }
+                                    title={dayjs(deal?.createdAt).format("MMM DD, YYYY - HH:mm")}
+                                    description={
+                                        <Space size={4}>
+                                            <Text strong>{item.user?.name}</Text>
+                                            <Text>
+                                                {item.action === "CREATE" ? "created" : "moved"}
+                                            </Text>
+                                            <Text strong>{deal?.title}</Text>
+                                            <Text>deal</Text>
+                                            <Text>{item.action === "CREATE" ? "in" : "to"}</Text>
+                                            <Text strong>{deal?.stage?.title || "Unassigned"}.</Text>
+                                        </Space>
+                                    }
+                                />
+                            </List.Item>
+                        )
+                    }}
+                />
+            )}
+        </Card>
+    )
 }
