bc5a1ceb4c6fae9438e01c88e468202ed0b47e43
Xenos Warlocks <139984318+XenosWarlocks@users.noreply.github.com>
2024-11-04 17:14:54 +0530

    Update form.tsx

--- src/routes/companies/edit/form.tsx
+++ src/routes/companies/edit/form.tsx
@@ -23,7 +23,6 @@ import type {
 import { getNameInitials } from "@/utilities";
 
 import { UPDATE_COMPANY_MUTATION } from "./queries";
-import value from '../../../../../../webapp/urban_wheels/types/image';
 
 export const CompanyForm = () => {
     const {
@@ -112,12 +111,87 @@ export const CompanyForm = () => {
                     <Select options={businessTypeOptions}/>
                 </Form.Item>
                 <Form.Item label="Country" name="country">
-                    <Select options={Country}/>
+                    <Select placeholder="Country"/>
                 </Form.Item>
                 <Form.Item label="Website" name="website">
-                    <Select options={Website}/>
+                    <Select placeholder="Website"/>
                 </Form.Item>
             </Form>
         </Edit>
     );
 };
+
+const companySizeOptions: {
+    label: string;
+    value: CompanySize;
+}[] = [
+    {
+        label: "Enterprise",
+        value: "ENTERPRISE",
+    },
+    {
+        label: "Large",
+        value: "LARGE",
+    },
+    {
+        label: "Medium",
+        value: "MEDIUM",
+    },
+    {
+        label: "Small",
+        value: "SMALL",
+    },
+];
+
+const industryOptions: {
+    label: string;
+    value: Industry;
+}[] = [
+    { label: "Aerospace", value: "AEROSPACE" },
+    { label: "Agriculture", value: "AGRICULTURE" },
+    { label: "Automotive", value: "AUTOMOTIVE" },
+    { label: "Chemicals", value: "CHEMICALS" },
+    { label: "Construction", value: "CONSTRUCTION" },
+    { label: "Defense", value: "DEFENSE" },
+    { label: "Education", value: "EDUCATION" },
+    { label: "Energy", value: "ENERGY" },
+    { label: "Financial Services", value: "FINANCIAL_SERVICES" },
+    { label: "Food and Beverage", value: "FOOD_AND_BEVERAGE" },
+    { label: "Government", value: "GOVERNMENT" },
+    { label: "Healthcare", value: "HEALTHCARE" },
+    { label: "Hospitality", value: "HOSPITALITY" },
+    { label: "Industrial Manufacturing", value: "INDUSTRIAL_MANUFACTURING" },
+    { label: "Insurance", value: "INSURANCE" },
+    { label: "Life Sciences", value: "LIFE_SCIENCES" },
+    { label: "Logistics", value: "LOGISTICS" },
+    { label: "Media", value: "MEDIA" },
+    { label: "Mining", value: "MINING" },
+    { label: "Nonprofit", value: "NONPROFIT" },
+    { label: "Other", value: "OTHER" },
+    { label: "Pharmaceuticals", value: "PHARMACEUTICALS" },
+    { label: "Professional Services", value: "PROFESSIONAL_SERVICES" },
+    { label: "Real Estate", value: "REAL_ESTATE" },
+    { label: "Retail", value: "RETAIL" },
+    { label: "Technology", value: "TECHNOLOGY" },
+    { label: "Telecommunications", value: "TELECOMMUNICATIONS" },
+    { label: "Transportation", value: "TRANSPORTATION" },
+    { label: "Utilities", value: "UTILITIES" },
+]
+
+const businessTypeOptions: {
+    label: string;
+    value: BusinessType;
+}[] = [
+    {
+        label: "B2B",
+        value: "B2B",
+    },
+    {
+        label: "B2C",
+        value: "B2C",
+    },
+    {
+        label: "B2G",
+        value: "B2G",
+    },
+];
